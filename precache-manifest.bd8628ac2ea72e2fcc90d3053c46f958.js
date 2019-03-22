1aa1395324b61f1f0ba2e2e4a96a42bd41c0faa3
Osvalam86 <valam.osvaldo@gmail.com>
2019-03-21 23:57:45 -0600

    primer commit

---
+++ precache-manifest.bd8628ac2ea72e2fcc90d3053c46f958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0072644fd7980b0749f",
+    "url": "/static/js/main.2388ca02.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "a0072644fd7980b0749f",
+    "url": "/static/css/main.39f0c581.chunk.css"
+  },
+  {
+    "revision": "269e20470fd2a686634cc0c94c552674",
+    "url": "/index.html"
+  }
+];
